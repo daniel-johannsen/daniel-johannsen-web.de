3aebc3d9862c281740ebf4b9706517b77a135124
Daniel Johannsen <daniel-johannsen@web.de>
2022-05-27 18:50:46 +0200

    TS coments

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,26 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  /**
+   * This function is used to show the overlay on the project image.
+   * @param x number
+   */
   showOverlay(x) {
     let id = 'overlay' + x;
     document.getElementById(id).classList.remove('d-none');
   }
 
+
+/**
+   * This function is used to show the overlay on the project image.
+   * @param x number
+   */
   removeOverlay(x) {
     let id = 'overlay' + x;
     document.getElementById(id).classList.add('d-none');
   }
 
+
+  /**
+   * This function is used to show all the Angular projects.
+   */
   showAngularProjects() {
     document.getElementById('kanbanProject').classList.add('d-none');
     document.getElementById('elPolloLoco').classList.add('d-none');
     document.getElementById('pokedex').classList.add('d-none');
   }
 
+
+  /**
+   * This function is used to show all the JavaScript projects.
+   */
   showJavaScriptProjects() {
     document.getElementById('homepage').classList.add('d-none');
   }
 
+
+  /**
+   * This function is used to show all the projects.
+   */
   showAllProjects() {
     document.getElementById('kanbanProject').classList.remove('d-none');
     document.getElementById('elPolloLoco').classList.remove('d-none');
